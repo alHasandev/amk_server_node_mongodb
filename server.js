6493c81882552f2fab54ce58dae1695ed1d4202c
alHasandev <alhasan99dev@gmail.com>
2020-09-16 23:02:16 +0800

    refresh cors config 4

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ db.once("open", () => console.log("Connected to mongodb database!"));
 
 // Use express middleware
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
